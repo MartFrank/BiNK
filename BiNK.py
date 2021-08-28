09d6adca965efbcf7a1f0b1587970f3d7bb39578
Martin Franklin <martin.franklin@cambsfire.gov.uk>
2021-08-28 14:15:21 +0100

    Update BiNK.py

--- BiNK.py
+++ BiNK.py
@@ -9,6 +9,8 @@ from datetime import datetime
 
 import pprint
 
+DEBUG = 0
+
 
 def read_data(filename):
     """
@@ -25,17 +27,25 @@ def read_data(filename):
         line = line.split(",")
         
         # convert Current Rent to float for sorting etc
-        line[-1] = float(line[-1])
+        current_rent = float(line[-1])
+        if DEBUG: print("Current Rent : ", current_rent)
+        line[-1] = current_rent
         
         ## convert Lease Years to int
-        line[-2] = int(line[-2])
+        lease_years = int(line[-2])
+        if DEBUG: print("Lease Years : ", lease_years)
+        line[-2] = lease_years
+        
+        ## convert End Date to datetime object 
+        end_date = datetime.strptime(line[-3], '%d %b %Y') 
+        if DEBUG: print("End Date : ", end_date)
+        line[-3] = end_date
         
         ## convert Start Date to datetime object
-        line[-3] = datetime.strptime(line[-3], '%d %b %Y')
-        
-        ## convert End Date to datetime object (02 Aug 2019 == 
-        line[-4] = datetime.strptime(line[-4], '%d %b %Y')
-        
+        start_date = datetime.strptime(line[-4], '%d %b %Y')
+        if DEBUG: print("Start Date : ", start_date)
+        line[-4] = start_date
+                
         ## append this line into the data_list 
         data_list.append(line)
     return data_list
@@ -45,6 +55,7 @@ def read_data(filename):
 def req1(data_list):
     """
     Requirement 1
+    
     Read in the attached file
         Produce a list sorted by Current Rent in ascending order
         Obtain the first 5 items from the resultant list and output to the console
@@ -53,6 +64,7 @@ def req1(data_list):
     index_number = -1
     sorted_data = sorted(data_list, key=lambda x:x[index_number]) 
     
+    print("Lowest 5 current rents :")
     ## print the top 5
     for row in sorted_data[:5]:
         print(row)
@@ -61,19 +73,40 @@ def req1(data_list):
 def req2(data_list):
     """
     Requirement 2
+    
     From the list of all mast data, create a new list of mast data with Lease Years = 25 years.
         Output the list to the console, including all data fields
         Output the total rent for all items in this list to the console
     """
-    pass
+    ## filter only the 25 year least row into a new list
+    twenty_five = [x for x in data_list if x[-2] == 25]
+    
+    print("Lease Years = 25 :")
+    total_rent = 0.0
+    for row in twenty_five:
+        total_rent = total_rent + row[-1]
+        print(row)
+    print("Total rent for 25 year lease : ", total_rent)
 
 def req3(data_list):
     """
     Requirement 3
     Create a dictionary containing tenant name and a count of masts for each tenant
         Output the dictionary to the console in a readable form
+    
+    
+    -5 is Tenant Name
     """
-    pass
+    tenant_masts = {}
+    for row in data_list:
+        tenant_name = row[-5]
+        if tenant_name in tenant_masts:
+            tenant_masts[tenant_name] = tenant_masts[tenant_name] + 1
+        else:
+            tenant_masts[tenant_name] = 1
+    print("Tenant Name & Mast Count : ")
+    for name, count in tenant_masts.items():
+        print("%-60s : %d" %(name, count))
 
 ## req 4
 #~ List the data for rentals with Lease Start Date between 1st June 1999 and 31st August 2007
@@ -92,10 +125,9 @@ if __name__=="__main__":
     data_list = read_data("Python Developer Test Dataset.csv")
     
     ## call them all  
-    req1(data_list)
-
-
-    req2(data_list)
+    #~ req1(data_list)
+    #~ req2(data_list)
+    req3(data_list)
 
 
 
